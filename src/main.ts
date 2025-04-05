7a163d9ca2f295a56e12baa03a0ee7d142eebaed
Azerus96 <gencinski1996@gmail.com>
2025-04-05 21:04:24 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 
+console.log('[main.ts] Создание приложения Vue...'); // <-- ЛОГ ИНИЦИАЛИЗАЦИИ
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.mount('#app')
+
+console.log('[main.ts] Приложение Vue смонтировано.'); // <-- ЛОГ МОНТИРОВАНИЯ
